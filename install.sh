33b812cbef9ea38b86a299bb6cb8de13381d5e6d
NCEX <73142334+NCEX@users.noreply.github.com>
2020-10-29 21:13:53 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -368,7 +368,7 @@
     
     # Compil Blocknotify
     cd ~
-    git clone https://github.com/Kudaraidee/yiimp
+    git clone https://github.com/NCEX/yiimp
     cd $HOME/yiimp/blocknotify
     sudo sed -i 's/tu8tu5/'$blckntifypass'/' blocknotify.cpp
     sudo make
